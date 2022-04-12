8400c46ab69e8171574663bd51f363a5e30dd3c2
FarinaFrancesco <farinafnc@outlook.it>
2022-04-12 12:46:41 +0200

    Aggiunte le classi Conference e DbConference

--- .idea/test/logic/ConferenceQueryController.php
+++ .idea/test/logic/ConferenceQueryController.php
@@ -1,35 +1,11 @@
 <?php
-include_once ('../logic/DbConn.php');
+include_once ('DbConference.php');
+
 class ConferenceQueryController
 {
-    static function conferenceFuture()
+    static function getConferenceName($acronimo, $anno_edizione)
     {
-        $sql = 'CALL ritornaConferenzeFuture(\'' . date('Y-m-d') . '\');';
-        $res = DbConn::getInstance()::getPDO() -> query($sql);
-        $output = $res -> fetchAll(PDO::FETCH_ASSOC);
-        $res -> closeCursor();
-        return $output;
+        return DbConference::getConference($acronimo, $anno_edizione)->getName();
     }
 
-    static function conferencePassed()
-    {
-        $sql = 'CALL ritornaConferenzePassate(\'' . date('Y-m-d') . '\');';
-        $res = DbConn::getInstance()::getPDO() -> query($sql);
-        $output = $res -> fetchAll(PDO::FETCH_ASSOC);
-        $res -> closeCursor();
-        return $output;
-    }
-
-    static function daysConference($acronimoConferenza,$annoEdizione)
-    {
-        if (isset($acronimoConferenza) && isset($annoEdizione)) {
-            $sql = 'CALL ritornaGiorniConferenza(\'' . $acronimoConferenza . '\',\'' . $annoEdizione . '\');';
-            $res = DbConn::getInstance()::getPDO()->query($sql);
-            $output = $res->fetchAll(PDO::FETCH_ASSOC);
-            $res->closeCursor();
-            return $output;
-        }else {
-            return null;
-        }
-    }
 }
