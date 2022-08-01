37f85f38935a53c3bc343fcda218ab9b0bae0465
spacexplorer-code <82576267+spacexplorer-code@users.noreply.github.com>
2022-08-01 16:59:41 +0200

    fixed navbar login logout
    sostituito href con un button per i vari utenti non admin per fixare il logout

--- templates/navbar.php
+++ templates/navbar.php
@@ -84,7 +84,7 @@ try {
                                 </div>
                             </li>
                             <li class="nav-item"><!--suppress HtmlDeprecatedAttribute -->
-                                <a name='logout' href="/pages/login.php" class="nav-link">logout</a></li>
+                                <button name='logout' class="nav-link">logout</button>
                         </ul>
                     </div>
                 </div>
@@ -111,13 +111,41 @@ try {
                                 </div>
                             </li>
                             <li class="nav-item"><!--suppress HtmlDeprecatedAttribute -->
-                                <a name='logout' href="/pages/login.php" class="nav-link">logout</a></li>
+                                <button name='logout' class="nav-link">logout</button>
                         </ul>
                     </div>
                 </div>
             </nav>
             <?php
             break;
+        case 'utente':
+             ?>
+            <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                            aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span> Menu
+                    </button>
+                    <div class="collapse navbar-collapse" id="ftco-nav">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item"><a href="/index.php" class="nav-link">Home</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown"
+                                   aria-haspopup="true" aria-expanded="false">Conferenze</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                                    <a class="dropdown-item" href="#">Prossime confereze</a>
+                                    <a class="dropdown-item" href="#">Conferenze passate</a>
+                                </div>
+                            </li>
+                            <li class="nav-item"><!--suppress HtmlDeprecatedAttribute -->
+                                <button name='logout' class="nav-link">logout</button>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <?php
+            break;
+
         default : ?>
             <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
                 <div class="container">
@@ -151,4 +179,4 @@ try {
 }
 include_once (sprintf("%s/templates/navbarScriptReference.html", $_SERVER["DOCUMENT_ROOT"]));
 
-?>
+?>;
