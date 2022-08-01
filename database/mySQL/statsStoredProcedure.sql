772259506d7e1bb87cd1096b38f6492c1b39b689
spacexplorer-code <82576267+spacexplorer-code@users.noreply.github.com>
2022-08-01 16:14:21 +0200

    added dashboard
    stats dashboard e relativi querycontroller e file sql

---
+++ database/mySQL/statsStoredProcedure.sql
@@ -0,0 +1,59 @@
+#STORED PROCEDURE PER LA VISUALIZZAZIONE DELLE STATISTICHE NELLA HOME PAGE
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaConferenzeRegistrate $$
+CREATE PROCEDURE contaConferenzeRegistrate()
+BEGIN
+SELECT count(*) AS numConferenzeRegistrate
+FROM CONFERENZA;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaConferenzeAttive $$
+CREATE PROCEDURE contaConferenzeAttive()
+BEGIN
+SELECT count(*) AS numConferenzeAttive
+FROM CONFERENZA
+WHERE statoSvolgimento = 'attiva';
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaUtentiTotali $$
+CREATE PROCEDURE contaUtentiTotali()
+BEGIN
+SELECT count(*) AS numUtentiTotali
+FROM UTENTE;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaUtentiAmministratori $$
+CREATE PROCEDURE contaUtentiAmministratori()
+BEGIN
+SELECT count(*) AS numUtentiAmministratori
+FROM UTENTE
+WHERE userName IN (SELECT userNameUtente FROM AMMINISTRATORE);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaUtentiSpeaker $$
+CREATE PROCEDURE contaUtentiSpeaker()
+BEGIN
+SELECT count(*) AS numUtentiSpeaker
+FROM UTENTE
+WHERE userName IN (SELECT userNameUtente FROM SPEAKER);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS contaUtentiPresenter $$
+CREATE PROCEDURE contaUtentiPresenter()
+BEGIN
+SELECT count(*) AS numUtentiPresenter
+FROM UTENTE
+WHERE userName IN (SELECT userNameUtente FROM PRESENTER);
+END $$
+DELIMITER ;
---
+++ logic/DbStats.php
@@ -0,0 +1,96 @@
+<?php
+include_once($_SERVER["DOCUMENT_ROOT"] . "/logic/DbConn.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/logic/Presentation.php");
+
+class DbStats
+{
+    public static function contaConferenzeRegistrate()
+
+    {
+        try {
+            $sql = 'CALL contaConferenzeRegistrate();';
+            $res = DbConn::getInstance()->query($sql);
+
+            $output = $res -> fetchAll(PDO::FETCH_ASSOC);
+            $res -> closeCursor();
+            if  (sizeof($output) > 0)
+            {
+                return $output;
+            } else
+            {
+                echo '<pre>
+
+                <p>nessuna conferenza resgistrata.</p>
+
+                </pre>';
+                return null;
+            }
+        } catch (Exception $e)
+        {
+            echo '<h1>HO PROVATO AD ESEGUIRE:</h1><p><b>' . $sql .'</b></p>';
+            echo $e;
+            return false;
+        }
+    }
+
+    public static function contaConferenzeAttive()
+
+    {
+        try {
+            $sql = 'CALL contaConferenzeAttive();';
+            $res = DbConn::getInstance()->query($sql);
+
+            $output = $res -> fetchAll(PDO::FETCH_ASSOC);
+            $res -> closeCursor();
+            if  (sizeof($output) > 0)
+            {
+                return $output;
+            } else
+            {
+                echo '<pre>
+
+                <p>nessuna conferenza attiva.</p>
+
+                </pre>';
+                return null;
+            }
+        } catch (Exception $e)
+        {
+            echo '<h1>HO PROVATO AD ESEGUIRE:</h1><p><b>' . $sql .'</b></p>';
+            echo $e;
+            return false;
+        }
+    }
+
+    public static function contaUtentiTotali()
+
+    {
+        try {
+            $sql = 'CALL contaUtentiTotali();';
+            $res = DbConn::getInstance()->query($sql);
+
+            $output = $res -> fetchAll(PDO::FETCH_ASSOC);
+            $res -> closeCursor();
+            if  (sizeof($output) > 0)
+            {
+                return $output;
+            } else
+            {
+                echo '<pre>
+
+                <p>nessun utente registrato.</p>
+
+                </pre>';
+                return null;
+            }
+        } catch (Exception $e)
+        {
+            echo '<h1>HO PROVATO AD ESEGUIRE:</h1><p><b>' . $sql .'</b></p>';
+            echo $e;
+            return false;
+        }
+    }
+
+
+}
+
