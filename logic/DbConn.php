d36777a46e536be1aeca485fa025eefd29787426
Giacomo Galletti <giacomo.galletti2@studio.unibo.it>
2022-08-19 03:07:59 +0200

    refactor e fix Singleton DbConn

--- logic/DbConn.php
+++ logic/DbConn.php
@@ -1,43 +1,16 @@
 <?php
 class DbConn {
-    private static $PDO;
-    private static $currentDbConn = null;
+    private static $instance;
 
-    private function __construct($PDO){
-        self::setPDO($PDO);
-        self::$PDO -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }
+    private function __construct(){}
 
-    public static function getInstance():PDO
+    public static function getInstance(): PDO
     {
-        try {
+        if (empty(self::$instance)) {
+            self::$instance = new PDO('mysql:host=127.0.0.1;dbname=confvirtual;charset=utf8', 'root', 'radice');
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-            if(self::$currentDbConn == null)
-            {
-                self::$currentDbConn = new DbConn(new PDO('mysql:host=127.0.0.1;dbname=confvirtual;charset=utf8','root','radice'));
-
-            }
-            return self::$currentDbConn::getPDO();
-        } catch (PDOException $exception){
-            header('Location: /templates/Error500.php');
-            self::close();
-            exit();
         }
-
+        return self::$instance;
     }
-
-    private static function getPDO(){
-        return  self::$PDO;
-    }
-
-    public static function close(){
-        self::$currentDbConn = null;
-        self::$PDO = null;
-    }
-
-    private static function setPDO($PDO): void
-    {
-        self::$PDO = $PDO;
-    }
-
 }
